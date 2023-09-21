51a0dba58a9dd81e87abc609864022740067e23c
Steve <wachirastevecollins@gmail.com>
2023-09-21 10:48:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+var bestCustomer; 
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+
+const leastFavoriteCustomer = "Steve";
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Wachira"; 
+}
+
+
